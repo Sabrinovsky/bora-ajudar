4f196d51a469fdf804f8d71df963011099cabc1a
Matheus <matheusouzatj@gmail.com>
2019-01-22 12:44:43 -0200

    minor updates in front

--- src/components/Header.js
+++ src/components/Header.js
@@ -43,11 +43,11 @@ class Header extends Component{
                             Campanhas
                         </Link>
                     </li>
-                    <li className='nav-item px-lg-4'>
+                    {false && <li className='nav-item px-lg-4'>
                         <Link to='/contato' className='nav-link text-uppercase text-expanded'>
                             Contato
                         </Link>
-                    </li>
+                    </li>}
                     {console.log(this.state)}
                     {this.state.admin==true &&
                         <li className='nav-item px-lg-4'>
